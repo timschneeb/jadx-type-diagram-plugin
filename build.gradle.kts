7368f0f72636756291d8497f5359f19067367f30
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-02-15 00:40:19 +0100

    chore: upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 	// auto update dependencies with 'useLatestVersions' task
 	id("se.patrikerdes.use-latest-versions") version "0.2.18"
-	id("com.github.ben-manes.versions") version "0.51.0"
+	id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 dependencies {
@@ -23,21 +23,21 @@ dependencies {
 		isChanging = isJadxSnapshot
 	}
 
-	implementation("org.slf4j:slf4j-api:2.0.13")
+	implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
 
-	implementation("org.netbeans.api:org-netbeans-api-visual:RELEASE220")
-	implementation("org.netbeans.api:org-openide-util:RELEASE220")
-	implementation("org.netbeans.api:org-openide-util-lookup:RELEASE220")
+	implementation("org.netbeans.api:org-netbeans-api-visual:RELEASE240")
+	implementation("org.netbeans.api:org-openide-util:RELEASE240")
+	implementation("org.netbeans.api:org-openide-util-lookup:RELEASE240")
 
-	implementation("com.formdev:flatlaf:3.4.1")
-	implementation("com.formdev:flatlaf-extras:3.4.1")
+	implementation("com.formdev:flatlaf:3.5.4")
+	implementation("com.formdev:flatlaf-extras:3.5.4")
 
 	implementation("com.google.code.findbugs:jsr305:3.0.2")
 
-	testImplementation("ch.qos.logback:logback-classic:1.4.14")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+	testImplementation("ch.qos.logback:logback-classic:1.5.16")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0-RC2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.0-RC2")
 }
 
 repositories {
